c28f1ea876808d17583d117ee1d0b5720335cb70
adil <adil.houdaibi.ext@allianz-trade.com>
2024-08-14 19:17:24 +0200

    feat: streamlit frontend evolution 2

--- front_end/app/main.py
+++ front_end/app/main.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import duckdb
 import streamlit as st
+import time
 
 st.set_page_config(page_title="Job Scraper", page_icon=":guardsman:", layout="wide")
 st.markdown(
@@ -10,7 +11,7 @@ st.markdown(
     .title {
         font-size: 36px;
         font-weight: bold;
-        color: #008080;
+        color: #FF0000;
         padding: 10px;
     }
     </style>
@@ -29,22 +30,28 @@ with st.sidebar:
 
 # Call the API when the user submits the form
 if submit_button:
-    with st.spinner('Wait for it...'):
+    starting_time = time.time()
+    
+    with st.spinner("And now, the moment you've been waiting for...✨✨✨"):
+        
         response = requests.post("http://scraper-app:80/scrape", json={
             "job_title": job_title,
             "contracts": [str(contract) for contract in contracts],
             "location": location
         })
         session_id = response.json()["session_id"]
-
         # Get the basic info and additional info from the API
         basic_info_response = requests.get(f"http://scraper-app:80/scrape/basic_info/{session_id}").json()[session_id]
         additional_info_response = requests.get(f"http://scraper-app:80/scrape/additional_info/{session_id}").json()[session_id]
+
+        scraping_elasped_time = time.time() - starting_time
 
         if not basic_info_response or not additional_info_response :
             st.write(f"No add found for your filters")
         
         else :
+            processing_start_time = time.time()
+
             # Parse the data into Pandas DataFrames
             basic_info = pd.DataFrame(basic_info_response)
             additional_info = pd.DataFrame(additional_info_response)
@@ -69,18 +76,35 @@ if submit_button:
                                                 group by skill
                                                 order by count(*) desc
                                                 LIMIT 10;''').fetchdf()
-            date_of_publication_repartition = conn.execute("SELECT date, COUNT(*) as count FROM basic_info GROUP BY date").fetchdf()
-            company_repartition = conn.execute("SELECT company, COUNT(*) as count FROM basic_info GROUP BY company").fetchdf()
-
+            date_of_publication_repartition = conn.execute('''SELECT date, COUNT(*) as count 
+                                                           FROM basic_info GROUP BY date''').fetchdf()
+            company_repartition = conn.execute('''SELECT company, COUNT(*) as count 
+                                               FROM basic_info 
+                                               GROUP BY company
+                                                order by  COUNT(*) desc LIMIT 10''').fetchdf()
+            
+            
+            ending_time = time.time() - starting_time
+            processing_elapsed_time = time.time() - processing_start_time
+            
+            
             # Display the KPIs in the Streamlit app
-            st.write(f"Number of ads: {number_of_ads}")
-            kpi_number_of_ads = st.columns(1)
-            kpi_number_of_ads.metric(
-                label="Nb of ads 🔢",
-                value=number_of_ads)
-            st.write("Most researched skills:")
-            st.bar_chart(most_researched_skills.set_index("skill"))
+            col1, col2, col3, col4 = st.columns(4)
+
+            # Display number of ads as a KPI
+            col1.metric(label="Number of ads 🔢", value=number_of_ads)
+            col2.metric(label="Elapsed time ⌛", value=f'{round(ending_time, 2)}sec')
+            col3.metric(label="Scraping elapsed time ⛏", value=f'{round(scraping_elasped_time, 2)}sec')
+            col4.metric(label="Processing elapsed time 🔄", value=f'{round(processing_elapsed_time, 2)}sec')
+
+            # Display most researched skills as a bar chart
+            st.write("TOP10 most popular skills:")
+            st.bar_chart(most_researched_skills.set_index("skill"),color='#FF0000')
+
+            # Display date of publication repartition as a scatter chart
             st.write("Date of publication repartition:")
-            st.scatter_chart(date_of_publication_repartition.set_index("date"))
-            st.write("Company repartition:")
-            st.bar_chart(company_repartition.set_index("company"))
+            st.line_chart(date_of_publication_repartition.set_index("date"),color='#FF0000')
+
+            # Display company repartition as a bar chart
+            st.write("TOP 10 companies with the most job offers:")
+            st.bar_chart(company_repartition.set_index("company"),color='#FF0000')
